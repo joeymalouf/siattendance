5a79a5b46526b6cf145bf0ea4c702c8b2bf08c3d
Joseph M. Malouf <jmmalouf@turing.cs.olemiss.edu>
2019-06-03 13:57:11 -0500

    using vue.js resource

--- back/SISession.php
+++ back/SISession.php
@@ -1,12 +1,13 @@
 <?php
 
-    require_once("session.php");
-    require_once("global_functions.php");
-    $mysqli = db_connection();
+    require_once("../session.php");
+    require_once("../global_functions.php");
 
     function getSession($sessionID) {
+
+    $mysqli = db_connection();
         $query_session = $mysqli->prepare("SELECT * FROM Session WHERE sessionid = ?");
-        $query_session->bind_param("i", $sessionID);
+$query_session->bind_param("i", $sessionID);
         $query_session->execute();
         $result_session = get_result($query_session);
 
@@ -15,9 +16,8 @@
             header("Location: ../front/index.php");
             exit;
         }
-        return $result_session;
+        print_r(json_encode($result_session));
     }
-
     if (isset($_POST['func']) && $_POST['func'] == 'getSession') {
         if (isset($_POST['sessionID'])) {
             getSession($_POST['sessionID']);
